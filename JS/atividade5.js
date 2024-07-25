3efab068d1c6411f7cfbf25fe08c20f9a5ba418e
Tayrel Preci Rodrigues <tayrelpreci@hotmail.com>
2024-07-25 20:48:00 -0300

    001

--- JS/atividade5.js
+++ JS/atividade5.js
@@ -2,6 +2,6 @@ var bol1 = prompt('qual sua nota no primeiro trimestre? ')
 var bol2 = prompt('qual sua nota no segundo trimestre? ')
 var bol3 = prompt('qual sua nota no terceiro trimestre? ')
 
-var media = (parseInt(bol1)+parseInt(bol2)+parseInt(bol3)) / 2
+var media = (parseInt(bol1)+parseInt(bol2)+parseInt(bol3)) / 3
 
 document.write("sua média é: "+media)
